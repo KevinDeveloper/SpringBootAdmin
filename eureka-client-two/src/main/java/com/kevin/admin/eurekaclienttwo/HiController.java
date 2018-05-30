2b6d950ca826447a4ee6459691ba62cac1b0878b
Kevin <Kevin_developer@163.com>
2018-05-30 15:09:33 +0800

    合入原因：使用spring boot admin示例，集成了eureka 、hystrix 和turbine

---
+++ eureka-client-two/src/main/java/com/kevin/admin/eurekaclienttwo/HiController.java
@@ -0,0 +1,28 @@
+package com.kevin.admin.eurekaclienttwo;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ClassName: HiController
+ * @Description:
+ * @Author: Kevin
+ * @Date: 2018/5/30 14:08
+ */
+@RestController
+public class HiController {
+    @Value("${server.port}")
+    String port;
+
+    @GetMapping("/hi")
+    @HystrixCommand(fallbackMethod = "hiError")
+    public String home(@RequestParam String name){
+        return "hi,"+name+", this port="+port;
+    }
+    public String hiError(String name){
+        return "hi,"+name+",this is error";
+    }
+}
